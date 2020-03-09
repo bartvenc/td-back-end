8e7b943a10ab8126a1888403c8385bc7a56e6ae0
Salman <salman.ahmed@no.experis.com>
2020-03-09 14:47:05 +0100

    Initialize commit

---
+++ src/main/java/no/experis/tbbackend/TbBackEndApplication.java
@@ -0,0 +1,13 @@
+package no.experis.tbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/experis/tbbackend/TbBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package no.experis.tbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
