4029c127b37fa0af38e7720b40125bd38a8a0820
Salman <salman.ahmed@no.experis.com>
2020-03-10 11:09:49 +0100

    Testing

--- src/main/java/no/experis/tbbackend/TbBackEndApplication.java
+++ src/main/java/no/experis/tbbackend/TbBackEndApplication.java
@@ -1,11 +1,14 @@
 package no.experis.tbbackend;
 
+import no.experis.tbbackend.Configurations.AppProperties;
 import no.experis.tbbackend.Models.User;
 import no.experis.tbbackend.Repositories.UserRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class TbBackEndApplication {
 
 	public static void main(String[] args) {
