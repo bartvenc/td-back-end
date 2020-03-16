60ea867ec5193f2c72522a2ffea34f82bbbaad53
Salman <salman.ahmed@no.experis.com>
2020-03-16 17:26:02 +0100

    Added new controller function getFindByID, it works!

--- src/main/java/no/experis/tbbackend/controller/UserController.java
+++ src/main/java/no/experis/tbbackend/controller/UserController.java
@@ -6,26 +6,37 @@ import no.experis.tbbackend.repository.UserRepository;
 import no.experis.tbbackend.security.CurrentUser;
 import no.experis.tbbackend.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    private UserRepository userRepo;
 
-    @GetMapping("/admin/me")
+   /* @GetMapping("/admin/me")
     @PreAuthorize("hasRole('ADMIN')")
     public User getAdminCurrentUserEmail(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.findByEmail(userPrincipal.getEmail()).orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
-
-    @GetMapping("/user/me")
+/*
+   @GetMapping("/user/me")
     public User getCurrentUserEmail(@CurrentUser UserPrincipal userPrincipal) {
-        System.out.println("CALLING /USER/ME");
         return userRepository.findById(userPrincipal.getId()).orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
+*/
+   @GetMapping("/user/{id}")
+   public User getUserById(@PathVariable(value="id") long id){
+       System.out.println("Calling /user/{ID}");
+       return userRepository.findById(id);
+   }
 }
 
