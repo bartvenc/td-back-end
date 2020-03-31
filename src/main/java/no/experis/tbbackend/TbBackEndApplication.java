a5fa7451275e4792c67191416bdc518a638b55d3
Salman <salman.ahmed@no.experis.com>
2020-03-31 12:15:38 +0200

    Uploading JavaDocs

--- src/main/java/no/experis/tbbackend/TbBackEndApplication.java
+++ src/main/java/no/experis/tbbackend/TbBackEndApplication.java
@@ -4,12 +4,18 @@ package no.experis.tbbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Tb back end application.
+ */
 @SpringBootApplication
 public class TbBackEndApplication {
 
-	public static void main(String[] args) {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
         SpringApplication.run(TbBackEndApplication.class, args);
-
-
     }
 }
